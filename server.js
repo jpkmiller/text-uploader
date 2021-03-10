c4176b48048d3f93f2fa20994a7824c79ff6a7bd
jpkmiller <j.mueller5021@gmail.com>
2021-03-10 21:00:31 +0100

    changed local json to mongodb. Hopefully it works also on heroku

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const rateLimit = require('express-rate-limit');
 const {param} = require('express-validator');
+const {MongoClient} = require('mongodb');
 const fs = require('fs');
 const path = require('path');
-const app = express()
 const cors = require('cors');
-const port = process.env.PORT || 8081
-
 const crypto = require('crypto');
 
-const HOURS_24 = 60 * 60 * 24 * 1000;
+const uri = process.env.MONGODB_URI;
+const DELETION_THRESHOLD = 60 * 60 * 24 * 1000;
 
 // INIT SERVER
+const app = express()
+const port = process.env.PORT || 8081
 app.listen(port, () => console.log('Server started on port', port))
 app.use(express.static('public'));
 app.use(cors())
@@ -26,18 +27,6 @@ const limiter = rateLimit({
 // RATE LIMIT
 app.use(limiter)
 
-// READ FILES
-let texts;
-try {
-    const texts_json = fs.readFileSync('texts.json').toString()
-    texts = JSON.parse(texts_json)
-} catch (err) {
-    console.log('Server created texts.json.')
-    fs.writeFileSync('texts.json', '{}')
-    const texts_json = fs.readFileSync('texts.json').toString()
-    texts = JSON.parse(texts_json)
-}
-
 
 app.get('/', (req, res) => {
     const htmlPath = path.join(__dirname + '/index.html')
@@ -46,60 +35,79 @@ app.get('/', (req, res) => {
 
 
 // UPLOAD TEXT AND RECEIVE HASH AS RESPONSE
-app.post('/upload/:text', param('text').isString().isLength({min: 1}).trim(), (req, res) => {
+app.post('/upload/:text', param('text').isString().isLength({min: 1}).trim(), async (req, res) => {
     const text = req.params.text
-    console.log('Server received text.')
+    console.log(`Server received text ${text}.`)
     const shasum = crypto.createHash('sha512')
     const textHash = shasum.update(text + crypto.randomInt(10000)).digest('hex')
-    console.log(`Server added text ${textHash}.`)
-    // deepcode ignore PrototypePollution: using param from express-validator
-    texts[textHash] = {
-        text: text,
-        created: new Date()
+
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    try {
+        await client.connect()
+        const database = client.db('texts')
+
+        const items = database.collection('items')
+        const created = new Date()
+        await items.insertOne({_id: textHash, text: text, created: created})
+        console.log(`Server uploaded text ${textHash} at ${created}.`)
+    } catch (err) {
+        console.error(err)
+    } finally {
+        await client.close()
     }
+
     // deepcode ignore XSS: using param from express-validator
     res.send(textHash)
-    // WRITE BACK FILE
-    fs.writeFileSync('texts.json', JSON.stringify(texts), "utf8")
 })
 
 
 // REQUEST TEXT BY PROVIDING A HASH
-app.get('/request/:hash', param('hash').isString().isLength({min: 1}).trim(), (req, res) => {
+app.get('/request/:hash', param('hash').isString().isLength({min: 1}).trim(), async (req, res) => {
     const hash = req.params.hash
-    console.log('Server received hash.')
-    if (texts.hasOwnProperty(hash)) {
-        const text = texts[hash].text
-        console.log(`Server returns text ${text} from ${hash}.`)
-        // deepcode ignore XSS: using param from express-validator
-        res.send(text)
-    } else {
-        console.log('Server did not find text.')
-        res.sendStatus(404)
+    console.log(`Server received hash ${hash}.`)
+
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    try {
+        await client.connect()
+        const database = client.db('texts');
+        const collection = database.collection('items');
+        // deepcode ignore Sqli: using param from express-validator
+        await collection.findOne({_id: hash})
+            .then(result => {
+                if (result) {
+                    // deepcode ignore XSS: using param from express-validator
+                    console.log(`Server found ${result.text}.`)
+                    // deepcode ignore XSS: using param from express-validator
+                    res.send(result.text)
+                } else {
+                    res.sendStatus(404)
+                }
+            })
+    } catch (err) {
+        console.error(err)
+    } finally {
+        await client.close()
     }
 })
 
 
 // DELETE TEXTS AFTER 24 HOURS
-const checktexts = () => {
-    const now = new Date()
-    const textHashes = Object.keys(texts)
-    let textHash;
-    for (let index = 0; index < textHashes.length; index++) {
-        textHash = textHashes[index]
-        if (texts.hasOwnProperty(textHash)) {
-            const date = Date.parse(texts[textHash].created)
-
-            // CHECK IF 24 HOURS HAVE PASSED
-            if (now - date >= HOURS_24) {
-                console.log(`Deleting text ${textHash}.`)
-                delete texts[textHash]
-            }
-        }
+const checktexts = async () => {
+    console.log('Server checking for old items.')
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    try {
+        await client.connect()
+        const database = client.db('texts')
+        const items = database.collection('items')
+        await items.deleteMany( { created : {"$lt" : new Date(Date.now() - DELETION_THRESHOLD) } })
+    } catch (err) {
+        console.error(err)
+    } finally {
+        await client.close()
     }
-    fs.writeFileSync('texts.json', JSON.stringify(texts), "utf8")
 }
 
 setInterval(() => {
     checktexts()
-}, HOURS_24)
+}, DELETION_THRESHOLD)
